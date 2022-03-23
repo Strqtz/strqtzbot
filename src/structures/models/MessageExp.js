5b8d066e3f9908c25c8deebab5aebf4e2a076338
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-23 14:44:58 -0400

    Update MessageExp.js

--- src/structures/models/MessageExp.js
+++ src/structures/models/MessageExp.js
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
 
-const { Schema } = mongoose;
+const {
+    Schema
+} = mongoose;
 
 const MessageExp = new Schema({
-  discordID: { type: String, required: true, unique: true },
-  exp: { type: Number, required: true, default: 0 },
-  level: { type: Number, required: true, default: 0 },
+    discordID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    exp: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    level: {
+        type: Number,
+        required: true,
+        default: 0
+    },
 });
 export default mongoose.model("messageexp", MessageExp);
