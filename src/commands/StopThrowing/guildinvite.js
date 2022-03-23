99cb1c6ae392ef1517ce993944ad60d721dc3e61
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-23 14:32:02 -0400

    Update guildinvite.js

--- src/commands/StopThrowing/guildinvite.js
+++ src/commands/StopThrowing/guildinvite.js
@@ -1,46 +1,49 @@
-import { AkairoMessage, Command } from "discord-akairo";
+import {
+    AkairoMessage,
+    Command
+} from "discord-akairo";
 
-import { Message } from "discord.js";
+import {
+    Message
+} from "discord.js";
 import cachios from "cachios";
-import { mc } from "../../index.js";
+import {
+    mc
+} from "../../index.js";
 
 export default class GuildinviteCommand extends Command {
-  constructor() {
-    super("guildinvite", {
-      aliases: ["guildinvite", "ginvite"],
-      description: {
-        content: "Shows information about the discord bot",
-        usage: "info",
-        examples: ["-info"],
-      },
-      channel: "guild",
-      args: [
-        {
-          id: "member",
-          type: "string",
-          description: "A member of the StopThrowing Guild",
-        },
-      ],
-      ownerOnly: true,
-      cooldown: 120000,
-      ratelimit: 1,
-    });
-  }
+    constructor() {
+        super("guildinvite", {
+            aliases: ["guildinvite", "ginvite"],
+            description: {
+                content: "Shows information about the discord bot",
+                usage: "info",
+                examples: ["-info"],
+            },
+            channel: "guild",
+            args: [{
+                id: "member",
+                type: "string",
+                description: "A member of the StopThrowing Guild",
+            }, ],
+            ownerOnly: true,
+            cooldown: 120000,
+            ratelimit: 1,
+        });
+    }
 
-  /**
-   * @param {Message} message
-   * @param {{member:string}} args
-   */
+    /**
+     * @param {Message} message
+     * @param {{member:string}} args
+     */
 
-  async exec(message, args) {
-    const uuidb4data = await cachios.get(
-      `https://api.mojang.com/users/profiles/minecraft/${args.member}`,
-      {
-        ttl: 60,
-      }
-    );
-    const uuid = uuidb4data.data;
+    async exec(message, args) {
+        const uuidb4data = await cachios.get(
+            `https://api.mojang.com/users/profiles/minecraft/${args.member}`, {
+                ttl: 60,
+            }
+        );
+        const uuid = uuidb4data.data;
 
-    mc.chat(`/g invite ${uuid.name}`);
-  }
-}
+        mc.chat(`/g invite ${uuid.name}`);
+    }
