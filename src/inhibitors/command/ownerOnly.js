d1878be8b6fca33abaa6202b5d75b7c3e9ef1472
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-23 14:43:19 -0400

    Update ownerOnly.js

--- src/inhibitors/command/ownerOnly.js
+++ src/inhibitors/command/ownerOnly.js
@@ -1,27 +1,34 @@
-import { AkairoMessage, Command, Inhibitor } from "discord-akairo";
-import { Message, MessageEmbed } from "discord.js";
+import {
+    AkairoMessage,
+    Command,
+    Inhibitor
+} from "discord-akairo";
+import {
+    Message,
+    MessageEmbed
+} from "discord.js";
 
 export default class SuperUser extends Inhibitor {
-  constructor() {
-    super("ownerOnly", {
-      reason: "ownerOnly",
-      category: "command",
-      type: "post",
-      priority: 20,
-    });
-  }
-
-  /**
-   *@param {Message | AkairoMessage} message
-   * @param {Command} command
-   */
-
-  async exec(message, command) {
-    if (command.ownerOnly) {
-      if (!this.client.isOwner(message.author)) {
-        return true;
-      }
+    constructor() {
+        super("ownerOnly", {
+            reason: "ownerOnly",
+            category: "command",
+            type: "post",
+            priority: 20,
+        });
     }
-    return false;
-  }
+
+    /**
+     *@param {Message | AkairoMessage} message
+     * @param {Command} command
+     */
+
+    async exec(message, command) {
+        if (command.ownerOnly) {
+            if (!this.client.isOwner(message.author)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
