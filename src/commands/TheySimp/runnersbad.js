848aa7e9e6c95199323f40a662c517138a3a3e05
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-23 14:31:02 -0400

    Update runnersbad.js

--- src/commands/TheySimp/runnersbad.js
+++ src/commands/TheySimp/runnersbad.js
@@ -1,27 +1,32 @@
-import { AkairoMessage, Command } from "discord-akairo";
-import { Message } from "discord.js";
+import {
+    AkairoMessage,
+    Command
+} from "discord-akairo";
+import {
+    Message
+} from "discord.js";
 
 export default class RunnersBadCommnad extends Command {
-  constructor() {
-    super("runnersbad", {
-      aliases: ["runnersbad"],
-      description: {
-        content: "FC on top",
-        usage: "runnersbad",
-        examples: ["-runnersbad"],
-      },
-      slash: true,
-      cooldown: 1000,
-      ratelimit: 1,
-    });
-  }
+    constructor() {
+        super("runnersbad", {
+            aliases: ["runnersbad"],
+            description: {
+                content: "FC on top",
+                usage: "runnersbad",
+                examples: ["-runnersbad"],
+            },
+            slash: true,
+            cooldown: 1000,
+            ratelimit: 1,
+        });
+    }
 
-  /**
-   * @param {Message| AkairoMessage} message
-   */
-  exec(message) {
-    return message.util.reply(
-      `Running Moment ` + "https://imgur.com/a/DjPne4S"
-    );
-  }
+    /**
+     * @param {Message| AkairoMessage} message
+     */
+    exec(message) {
+        return message.util.reply(
+            `Running Moment ` + "https://imgur.com/a/DjPne4S"
+        );
+    }
 }
