1be06084da1d72b07b3f0ec2d1891ef37228bafe
Riceblades11 <96640977+Riceblade@users.noreply.github.com>
2022-03-23 14:48:08 -0400

    Update ready.js

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,33 +1,41 @@
-import { Listener } from "discord-akairo";
+import {
+    Listener
+} from "discord-akairo";
 import moment from "moment";
-import { MessageEmbed, Guild } from "discord.js";
+import {
+    MessageEmbed,
+    Guild
+} from "discord.js";
 import schedule from "node-schedule";
 import cachios from "cachios";
 import wait from "wait";
 
 export default class readyListener extends Listener {
-  constructor() {
-    super("ready", {
-      emitter: "client",
-      event: "ready",
-    });
-  }
+    constructor() {
+        super("ready", {
+            emitter: "client",
+            event: "ready",
+        });
+    }
 
-  /**
-   * @param {Guild} guild
-   */
-  async exec(guild) {
-    this.client.user.setPresence({
-      activities: [{ name: "You Sleep", type: "WATCHING" }],
-      status: "dnd",
-    });
-    this.client.logging.log(
-      "info",
-      `${this.client.user.tag} is online. Watching ${this.client.guilds.cache.size} servers. ${this.client.ws.shards.size} shards.`
-    );
-    this.client.logging.log(
-      "info",
-      `${this.client.commandHandler.modules.size} Commands Loaded. ${this.client.listenerHandler.modules.size} Listeners Loaded. ${this.client.inhibitorHandler.modules.size} Inhibitors Loaded.`
-    );
-  }
+    /**
+     * @param {Guild} guild
+     */
+    async exec(guild) {
+        this.client.user.setPresence({
+            activities: [{
+                name: "You Sleep",
+                type: "WATCHING"
+            }],
+            status: "dnd",
+        });
+        this.client.logging.log(
+            "info",
+            `${this.client.user.tag} is online. Watching ${this.client.guilds.cache.size} servers. ${this.client.ws.shards.size} shards.`
+        );
+        this.client.logging.log(
+            "info",
+            `${this.client.commandHandler.modules.size} Commands Loaded. ${this.client.listenerHandler.modules.size} Listeners Loaded. ${this.client.inhibitorHandler.modules.size} Inhibitors Loaded.`
+        );
+    }
 }
